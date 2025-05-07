aaa0a597635d7f4f1ddceb1c4a3386a6a4f42bed
aadeina <amarmed4500@gmail.com>
2025-05-07 14:28:05 +0100

    feat: add pagination to internship list API and fix endpoint path usage

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,  # You can change this number as needed
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 SIMPLE_JWT = {
